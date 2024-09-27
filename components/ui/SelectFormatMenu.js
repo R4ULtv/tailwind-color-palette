3dbafb33563da21580853dc0c40195f495483b01
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-27 20:54:12 +0200

    fix style

--- components/ui/SelectFormatMenu.js
+++ components/ui/SelectFormatMenu.js
@@ -31,11 +31,14 @@ export default function SelectFormatMenu() {
           <ListboxOption
             key={item.name}
             value={item.name}
-            className="group flex items-center justify-between rounded-md py-1.5 px-3 select-none data-[focus]:bg-zinc-800 data-[selected]:text-zinc-200 text-zinc-300"
+            className="group flex items-center justify-between gap-8 rounded-md py-1.5 px-3 select-none data-[focus]:bg-zinc-800 text-zinc-200"
           >
             <div className="flex cursor-default items-center gap-2">
               <div className="text-sm group-data-[selected]:font-semibold">
-                {item.name}
+                {item.name}{" "}
+                <span className="text-xs font-normal text-zinc-300">
+                  {item.example}
+                </span>
               </div>
             </div>
             <CheckIcon className="invisible size-4 group-data-[selected]:visible" />
