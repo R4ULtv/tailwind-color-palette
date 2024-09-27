5c47e8ec0ce8768db989b40b47a3fbd6464def0d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-27 20:53:40 +0200

    changed button

--- app/page.js
+++ app/page.js
@@ -1,67 +1,26 @@
-import React from "react";
-import Link from "next/link";
-import { PaintBrushIcon, SwatchIcon } from "@heroicons/react/16/solid";
-
 import { colors } from "@/lib/colors";
 
-import { ColorsProvider } from "@/components/providers/ColorsContext";
-import ColorButton from "@/components/ui/ColorButton";
+import { ColorButtonTailwind } from "@/components/ui/ColorButton";
 import SelectFormatMenu from "@/components/ui/SelectFormatMenu";
 
 export default function Home() {
   return (
-    <ColorsProvider>
-      <main className="max-w-screen-2xl py-8 sm:py-16 px-4 mx-auto">
-        <div className="relative">
-          <h1 className="text-2xl sm:text-3xl text-zinc-100 font-bold z-10 relative">
-            Tailwind Color Palette
-          </h1>
-          <p className="text-sm sm:text-base text-zinc-300 font-medium mt-1.5 z-10 relative">
-            Tailwind CSS colors in HSL, RGB, and HEX formats.
-          </p>
-
-          <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 mt-4 z-10 relative">
-            <a
-              href="#colors"
-              className="bg-zinc-200 text-zinc-900 px-2 py-1.5 rounded-lg text-sm font-medium flex items-center gap-1 w-full sm:w-auto justify-center sm:justify-start"
-            >
-              <SwatchIcon className="size-4" />
-              Browse Colors
-            </a>
-            <Link
-              href="/create-palette"
-              className="text-zinc-200 px-2 py-1.5 rounded-lg text-sm font-medium flex items-center gap-1 w-full sm:w-auto justify-center sm:justify-start"
-            >
-              <PaintBrushIcon className="size-4" />
-              Create Palette
-            </Link>
-          </div>
-
-          <div className="w-full h-full absolute inset-0 overflow-hidden blur-3xl">
-            <div className="absolute rounded-full bg-indigo-500 opacity-50 h-20 w-36 translate-x-full" />
-            <div className="absolute rounded-full bg-purple-500 opacity-50 h-20 w-36 translate-x-1/2 translate-y-1/2" />
-            <div className="absolute rounded-full bg-pink-500 opacity-50 h-20 w-36" />
-          </div>
-        </div>
-
-        <div className="mt-8 sm:mt-4 pt-4 space-y-3" id="colors">
-          <div className="flex items-center justify-end">
-            <SelectFormatMenu />
-          </div>
-          <div className="grid gap-4 sm:gap-6 w-full">
-            {Object.keys(colors).map((color) => (
-              <div
-                className="flex flex-col sm:flex-row gap-2 overflow-x-auto border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2"
-                key={color}
-              >
-                {colors[color].map((item) => (
-                  <ColorButton key={item.scale} item={item} color={color} />
-                ))}
-              </div>
+    <div className="mt-8 sm:mt-4 pt-4 space-y-3" id="colors">
+      <div className="flex items-center justify-end">
+        <SelectFormatMenu />
+      </div>
+      <div className="grid gap-4 sm:gap-6 w-full">
+        {Object.keys(colors).map((color) => (
+          <div
+            className="flex flex-col sm:flex-row gap-2 overflow-x-auto border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2"
+            key={color}
+          >
+            {colors[color].map((item) => (
+              <ColorButtonTailwind key={item.scale} item={item} color={color} />
             ))}
           </div>
-        </div>
-      </main>
-    </ColorsProvider>
+        ))}
+      </div>
+    </div>
   );
 }
--- components/ui/ColorButton.js
+++ components/ui/ColorButton.js
@@ -5,7 +5,7 @@ import { Button, Transition } from "@headlessui/react";
 import { ClipboardDocumentCheckIcon } from "@heroicons/react/24/solid";
 import { useColors } from "@/components/providers/ColorsContext";
 
-export default function ColorButton({ item, color }) {
+export function ColorButtonTailwind({ item, color }) {
   const { format } = useColors();
   const [copiedText, setCopiedText] = useState("");
 
@@ -40,7 +40,45 @@ export default function ColorButton({ item, color }) {
           </div>
         </Transition>
       </div>
-      <div className="text-sm font-medium text-zinc-400 mt-1.5 group-hover:text-zinc-200 truncate max-w-24 mx-auto">
+      <div className="text-sm font-medium text-zinc-400 mt-1.5 group-hover:text-zinc-200 truncate max-w-28 mx-auto">
+        {copyValue}
+      </div>
+    </Button>
+  );
+}
+
+export function ColorButtonPalette({ color }) {
+  const { format } = useColors();
+  const [copiedText, setCopiedText] = useState("");
+
+  const copyValue = useMemo(
+    () => (color[format]),
+    [format, color]
+  );
+
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard.writeText(copyValue).then(() => {
+      setCopiedText(copyValue);
+      setTimeout(() => setCopiedText(""), 1500);
+    });
+  }, [copyValue]);
+
+  return (
+    <Button
+      onClick={copyToClipboard}
+      className="h-full flex-1 transition duration-75 data-[hover]:border-zinc-600 data-[focus]:border-zinc-500 outline-none group"
+    >
+      <div
+        className="rounded-xl aspect-[5/1] sm:aspect-[2/1] w-auto h-auto relative"
+        style={{ backgroundColor: color.hex }}
+      >
+        <Transition show={!!copiedText}>
+          <div className="absolute inset-0 flex items-center justify-center transform transition duration-75 ease-in-out data-[closed]:opacity-0 group/icon">
+            <ClipboardDocumentCheckIcon className="size-9 group-data-[closed]/icon:scale-50 text-zinc-200 p-2 bg-zinc-800 rounded-xl" />
+          </div>
+        </Transition>
+      </div>
+      <div className="text-sm font-medium text-zinc-400 mt-1.5 group-hover:text-zinc-200 truncate max-w-28 mx-auto">
         {copyValue}
       </div>
     </Button>
