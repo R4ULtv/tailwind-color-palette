e990ab86d7beecd8016a3abf0d3f25cf97e1aacd
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-28 15:57:53 +0200

    code optimization

--- components/ui/ColorButton.js
+++ components/ui/ColorButton.js
@@ -5,14 +5,8 @@ import { Button, Transition } from "@headlessui/react";
 import { ClipboardDocumentCheckIcon } from "@heroicons/react/24/solid";
 import { useColors } from "@/components/providers/ColorsContext";
 
-export function ColorButtonTailwind({ item, color }) {
-  const { format } = useColors();
+const CopyButton = ({ copyValue, backgroundColor, textColor }) => {
   const [copiedText, setCopiedText] = useState("");
-
-  const copyValue = useMemo(
-    () => (format === "className" ? `${color}-${item.scale}` : item[format]),
-    [format, color, item]
-  );
 
   const copyToClipboard = useCallback(() => {
     navigator.clipboard.writeText(copyValue).then(() => {
@@ -21,8 +15,6 @@ export function ColorButtonTailwind({ item, color }) {
     });
   }, [copyValue]);
 
-  const textColor = item.scale > 500 ? "text-zinc-200" : "text-zinc-800";
-
   return (
     <Button
       onClick={copyToClipboard}
@@ -30,12 +22,12 @@ export function ColorButtonTailwind({ item, color }) {
     >
       <div
         className="rounded-xl flex-1 aspect-[5/1] sm:aspect-[2/1] w-full h-auto relative"
-        style={{ backgroundColor: item.hex }}
+        style={{ backgroundColor }}
       >
         <Transition show={!!copiedText}>
           <div className="absolute inset-0 flex items-center justify-center transform transition duration-75 ease-in-out data-[closed]:opacity-0 group/icon">
             <ClipboardDocumentCheckIcon
-              className={`size-6 group-data-[closed]/icon:scale-50 ${textColor}`}
+              className={`size-9 group-data-[closed]/icon:scale-50 p-2 rounded-xl ${textColor}`}
             />
           </div>
         </Transition>
@@ -45,39 +37,37 @@ export function ColorButtonTailwind({ item, color }) {
       </div>
     </Button>
   );
+};
+
+export function ColorButtonTailwind({ item, color }) {
+  const { format } = useColors();
+  const copyValue = useMemo(
+    () => (format === "className" ? `${color}-${item.scale}` : item[format]),
+    [format, color, item]
+  );
+  const textColor =
+    item.scale < 500
+      ? "text-zinc-200 bg-zinc-800"
+      : "text-zinc-800 bg-zinc-200";
+
+  return (
+    <CopyButton
+      copyValue={copyValue}
+      backgroundColor={item.hex}
+      textColor={textColor}
+    />
+  );
 }
 
 export function ColorButtonPalette({ color }) {
   const { format } = useColors();
-  const [copiedText, setCopiedText] = useState("");
-
   const copyValue = useMemo(() => color[format], [format, color]);
 
-  const copyToClipboard = useCallback(() => {
-    navigator.clipboard.writeText(copyValue).then(() => {
-      setCopiedText(copyValue);
-      setTimeout(() => setCopiedText(""), 1500);
-    });
-  }, [copyValue]);
-
   return (
-    <Button
-      onClick={copyToClipboard}
-      className="w-auto h-auto flex-1 flex flex-col gap-1.5 transition duration-75 data-[hover]:border-zinc-600 data-[focus]:border-zinc-500 outline-none group"
-    >
-      <div
-        className="rounded-xl flex-1 aspect-[5/1] sm:aspect-[2/1] w-full h-auto relative"
-        style={{ backgroundColor: color.hex }}
-      >
-        <Transition show={!!copiedText}>
-          <div className="absolute inset-0 flex items-center justify-center transform transition duration-75 ease-in-out data-[closed]:opacity-0 group/icon">
-            <ClipboardDocumentCheckIcon className="size-9 group-data-[closed]/icon:scale-50 text-zinc-200 p-2 bg-zinc-800 rounded-xl" />
-          </div>
-        </Transition>
-      </div>
-      <div className="text-sm font-medium text-zinc-400 group-hover:text-zinc-200 truncate w-full px-2">
-        {copyValue}
-      </div>
-    </Button>
+    <CopyButton
+      copyValue={copyValue}
+      backgroundColor={color.hex}
+      textColor="text-zinc-200 bg-zinc-800"
+    />
   );
 }
