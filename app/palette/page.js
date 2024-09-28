9428c9afa16a64270f9cd203fdf71af141011d83
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-28 16:33:15 +0200

    add taildwindcss export

--- app/palette/page.js
+++ app/palette/page.js
@@ -2,13 +2,21 @@ import { memo } from "react";
 import { palette } from "@/lib/palette";
 import SelectFormatMenu from "@/components/ui/SelectFormatMenu";
 import { ColorButtonPalette } from "@/components/ui/ColorButton";
+import TailwindExport from "@/components/ui/TailwindExport";
 
 const PaletteItem = memo(({ item }) => (
   <div className="flex flex-col gap-1.5 overflow-x-auto border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2">
-    <div className="text-sm text-zinc-200 font-medium ml-1.5">
-      {item.name} -{" "}
-      <a href={`https://github.com/${item.author}`}>{item.author}</a>
+    <div className="flex items-center justify-between gap-8 my-1">
+      <div className="text-sm text-zinc-200 font-medium ml-1.5">
+        {item.name} -{" "}
+        <a href={`https://github.com/${item.author}`}>{item.author}</a>
+      </div>
+      <TailwindExport
+        className={item.name.toLowerCase()}
+        colors={item.colors}
+      />
     </div>
+
     <div className="flex flex-col sm:flex-row gap-2">
       {item.colors.map((color, index) => (
         <ColorButtonPalette key={`${item.name}-${index}`} color={color} />
