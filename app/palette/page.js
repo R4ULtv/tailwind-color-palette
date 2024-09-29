ddf55dc1b4b8badd8e1f6b20f30893712c231bbb
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-29 09:56:15 +0200

    fix authors

--- app/palette/page.js
+++ app/palette/page.js
@@ -5,21 +5,38 @@ import { ColorButtonPalette } from "@/components/ui/ColorButton";
 import TailwindExport from "@/components/ui/TailwindExport";
 
 const PaletteItem = memo(({ item }) => (
-  <div className="flex flex-col gap-1.5 overflow-x-auto border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2">
+  <div className="flex flex-col gap-1.5 border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2">
     <div className="flex items-center justify-between gap-8 my-1">
       <div className="text-sm text-zinc-200 font-medium ml-1.5">
         {item.name} -{" "}
-        <a href={`https://github.com/${item.author}`}>{item.author}</a>
+        {item.authors.map((author, index) => (
+          <span key={index}>
+            <a
+              href={`https://github.com/${author}`}
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              @{author}
+            </a>
+            {index < item.authors.length - 1 && ", "}
+          </span>
+        ))}
       </div>
-      <TailwindExport
-        className={item.name.toLowerCase()}
-        colors={item.colors}
-      />
+      <div className="flex items-center gap-1">
+        <TailwindExport
+          className={item.name.toLowerCase()}
+          colors={item.colors}
+        />
+      </div>
     </div>
 
-    <div className="flex flex-col sm:flex-row gap-2">
+    <div className="flex flex-col sm:flex-row gap-2 relative">
       {item.colors.map((color, index) => (
-        <ColorButtonPalette key={`${item.name}-${index}`} color={color} />
+        <ColorButtonPalette
+          key={`${item.name}-${index}`}
+          color={color}
+          palette={item}
+        />
       ))}
     </div>
   </div>
@@ -30,7 +47,7 @@ PaletteItem.displayName = "PaletteItem";
 export default function PaletteList() {
   return (
     <div className="mt-8 sm:mt-4 pt-4 space-y-3">
-      <div className="flex items-center justify-end">
+      <div className="flex items-center justify-end gap-2">
         <SelectFormatMenu />
       </div>
       <div className="grid gap-4 sm:gap-6">
