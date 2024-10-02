138dca3a35dd5b230f5f3f64f75e78dfdab0fe83
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-02 16:25:15 +0200

    add edit mode

--- components/providers/ColorsContext.js
+++ components/providers/ColorsContext.js
@@ -12,6 +12,10 @@ const ColorsContext = createContext();
 
 export function ColorsProvider({ children }) {
   const [format, setFormat] = useState("className");
+  const [localPalette, setLocalPalette] = useState([]);
+  const [editPalette, setEditPalette] = useState(null);
+  const [isNewPalette, setIsNewPalette] = useState(false);
+
   const formatList = [
     { name: "className", shortcut: "c", example: "slate-100" },
     { name: "hex", shortcut: "h", example: "#f1f5f9" },
@@ -31,6 +35,7 @@ export function ColorsProvider({ children }) {
 
   const handleKeyDown = useCallback(
     (e) => {
+      if (editPalette) return;
       if (!format) return;
 
       formatList.forEach((item) => {
@@ -62,7 +67,7 @@ export function ColorsProvider({ children }) {
         localStorage.setItem("colors-format", formatList[nextIndex].name);
       }
     },
-    [format, setFormat]
+    [format, setFormat, editPalette]
   );
 
   useEffect(() => {
@@ -70,8 +75,27 @@ export function ColorsProvider({ children }) {
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, [handleKeyDown]);
 
+  useEffect(() => {
+    const storedPalette = localStorage.getItem("colors-palette");
+    if (storedPalette) {
+      setLocalPalette(JSON.parse(storedPalette));
+    }
+  }, []);
+
   return (
-    <ColorsContext.Provider value={{ formatList, format, setFormat }}>
+    <ColorsContext.Provider
+      value={{
+        formatList,
+        format,
+        setFormat,
+        localPalette,
+        setLocalPalette,
+        editPalette,
+        setEditPalette,
+        isNewPalette,
+        setIsNewPalette,
+      }}
+    >
       {children}
     </ColorsContext.Provider>
   );
---
+++ components/ui/EditMode.js
@@ -0,0 +1,325 @@
+"use client";
+
+import { memo, useCallback } from "react";
+import {
+  BackspaceIcon,
+  BookmarkIcon,
+  PencilIcon,
+  PlusCircleIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
+import {
+  Button,
+  Field,
+  Input,
+  Label,
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+} from "@headlessui/react";
+import { HexColorPicker } from "react-colorful";
+
+import { ColorButtonPalette } from "@/components/ui/ColorButton";
+import { TailwindExport, GithubExport } from "@/components/ui/TailwindExport";
+import { useColors } from "@/components/providers/ColorsContext";
+import {
+  convertHexColor,
+  generateRandomHexColor,
+  isValidColorFormat,
+} from "@/utils/colors";
+
+export const PaletteItem = memo(({ item }) => {
+  const {
+    editPalette,
+    setEditPalette,
+    localPalette,
+    setLocalPalette,
+    setIsNewPalette,
+  } = useColors();
+
+  const isEditing = editPalette?.uuid === item.uuid;
+
+  const handleSave = useCallback(() => {
+    if (isEditing) {
+      if (
+        !editPalette.name ||
+        !editPalette.authors.length ||
+        !editPalette.colors.length
+      ) {
+        return;
+      }
+
+      const updatedPalette = localPalette.map((palette) =>
+        palette.uuid === item.uuid ? editPalette : palette
+      );
+
+      if (!updatedPalette.some((palette) => palette.uuid === item.uuid)) {
+        updatedPalette.push(editPalette);
+      }
+
+      localStorage.setItem("colors-palette", JSON.stringify(updatedPalette));
+      setEditPalette(null);
+      setIsNewPalette(false);
+      setLocalPalette(updatedPalette);
+    } else {
+      setEditPalette(item);
+    }
+  }, [
+    isEditing,
+    editPalette,
+    item,
+    localPalette,
+    setEditPalette,
+    setIsNewPalette,
+    setLocalPalette,
+  ]);
+
+  const handleEditName = useCallback(
+    (e) => {
+      setEditPalette((prev) => ({ ...prev, name: e.target.value }));
+    },
+    [setEditPalette]
+  );
+
+  const handleEditAuthors = useCallback(
+    (e) => {
+      setEditPalette((prev) => ({
+        ...prev,
+        authors: e.target.value.split(","),
+      }));
+    },
+    [setEditPalette]
+  );
+
+  const handleAddColor = useCallback(() => {
+    const randomColor = generateRandomHexColor();
+    setEditPalette((prev) => ({
+      ...prev,
+      colors: [
+        ...prev.colors,
+        {
+          hex: randomColor,
+          rgb: convertHexColor(randomColor, "rgb"),
+          hsl: convertHexColor(randomColor, "hsl"),
+          className: "",
+        },
+      ],
+    }));
+  }, [setEditPalette]);
+
+  const handleRemoveColor = useCallback(() => {
+    setEditPalette((prev) => ({
+      ...prev,
+      colors: prev.colors.slice(0, -1),
+    }));
+  }, [setEditPalette]);
+
+  const handleDelete = useCallback(() => {
+    setEditPalette(null);
+    setLocalPalette((prev) =>
+      prev.filter((palette) => palette.uuid !== item.uuid)
+    );
+    localStorage.setItem(
+      "colors-palette",
+      JSON.stringify(
+        localPalette.filter((palette) => palette.uuid !== item.uuid)
+      )
+    );
+  }, [item.uuid, setEditPalette, setLocalPalette, localPalette]);
+
+  const renderEditFields = () => (
+    <div className="flex gap-2">
+      <Field className="max-w-60">
+        <Label>Palette Name</Label>
+        <Input
+          required
+          type="text"
+          placeholder="Sunset..."
+          className="mt-2 py-1 px-2 text-start font-medium bg-transparent placeholder:text-zinc-500 text-zinc-300 border border-zinc-700 rounded-lg w-full outline-none"
+          value={editPalette.name}
+          onChange={handleEditName}
+        />
+      </Field>
+      <Field className="max-w-60">
+        <Label>Authors (Github Username)</Label>
+        <Input
+          required
+          type="text"
+          placeholder="r4ultv, ..."
+          className="mt-2 py-1 px-2 text-start font-medium bg-transparent placeholder:text-zinc-500 text-zinc-300 border border-zinc-700 rounded-lg w-full outline-none"
+          value={editPalette.authors.join(", ")}
+          onChange={handleEditAuthors}
+        />
+      </Field>
+    </div>
+  );
+
+  const renderColorPicker = (color, index) => (
+    <Popover key={index} className="relative w-auto h-auto flex-1">
+      <div className="w-full h-full flex flex-col gap-1.5">
+        <PopoverButton
+          className="rounded-xl flex-1 aspect-[5/1] sm:aspect-[2/1] w-full h-auto max-h-40"
+          style={{ backgroundColor: color.hex }}
+        />
+        <Input
+          required
+          type="text"
+          placeholder="Color Name"
+          value={color.className}
+          onChange={(e) =>
+            setEditPalette((prev) => ({
+              ...prev,
+              colors: prev.colors.map((c, i) =>
+                i === index ? { ...c, className: e.target.value } : c
+              ),
+            }))
+          }
+          className="text-sm mt-2 py-1 text-center font-medium bg-transparent placeholder:text-zinc-500 text-zinc-300 border border-zinc-700 rounded-lg w-full outline-none"
+        />
+      </div>
+      <PopoverPanel
+        transition
+        anchor="top"
+        className="z-50 transition duration-150 ease-in-out translate-y-2 data-[closed]:opacity-0 data-[closed]:scale-50 overflow-hidden bg-zinc-900 border border-zinc-700 rounded-xl pt-4 px-4 pb-2"
+      >
+        <HexColorPicker
+          color={color.hex}
+          onChange={(newColor) =>
+            setEditPalette((prev) => ({
+              ...prev,
+              colors: prev.colors.map((c, i) =>
+                i === index
+                  ? {
+                      ...c,
+                      hex: newColor,
+                      rgb: convertHexColor(newColor, "rgb"),
+                      hsl: convertHexColor(newColor, "hsl"),
+                    }
+                  : c
+              ),
+            }))
+          }
+        />
+        <Input
+          required
+          type="text"
+          className="text-sm mt-2 py-1 text-center font-medium bg-transparent text-zinc-300 border border-zinc-700 rounded-lg w-full outline-none"
+          value={color.hex}
+          onChange={(e) => {
+            if (isValidColorFormat(e.target.value)) {
+              setEditPalette((prev) => ({
+                ...prev,
+                colors: prev.colors.map((c, i) =>
+                  i === index ? { ...c, hex: e.target.value } : c
+                ),
+              }));
+            }
+          }}
+        />
+      </PopoverPanel>
+    </Popover>
+  );
+
+  return (
+    <div className="flex flex-col gap-1.5 border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 my-1">
+        <div className="text-sm text-zinc-200 font-medium ml-1.5">
+          {isEditing ? (
+            renderEditFields()
+          ) : (
+            <>
+              {item.name} -{" "}
+              {item.authors.map((author, index) => (
+                <span key={index}>
+                  <a
+                    href={`https://github.com/${author}`}
+                    target="blank"
+                    rel="noopener noreferrer"
+                  >
+                    @{author}
+                  </a>
+                  {index < item.authors.length - 1 && ", "}
+                </span>
+              ))}
+            </>
+          )}
+        </div>
+        <div className="flex items-center gap-1">
+          <Button
+            onClick={handleSave}
+            className="p-1.5 rounded-lg border border-zinc-700 text-zinc-200 group flex items-center gap-1 data-[hover]:border-zinc-600 transition duration-75 ease-in-out"
+          >
+            {isEditing ? (
+              <>
+                <BookmarkIcon className="size-3.5" />
+                <span className="text-xs font-medium hidden sm:block">
+                  Save
+                </span>
+              </>
+            ) : (
+              <>
+                <PencilIcon className="size-3.5" />
+                <span className="text-xs font-medium hidden sm:block">
+                  Edit
+                </span>
+              </>
+            )}
+          </Button>
+          {isEditing && (
+            <>
+              <Button
+                onClick={handleAddColor}
+                className="p-1.5 rounded-lg border border-zinc-700 text-zinc-200 group flex items-center gap-1 data-[hover]:border-zinc-600 transition duration-75 ease-in-out"
+              >
+                <PlusCircleIcon className="size-3.5" />
+                <span className="text-xs font-medium hidden sm:block">
+                  Add Color
+                </span>
+              </Button>
+              <Button
+                onClick={handleRemoveColor}
+                className="p-1.5 rounded-lg border border-zinc-700 text-zinc-200 group flex items-center gap-1 data-[hover]:border-zinc-600 transition duration-75 ease-in-out"
+              >
+                <BackspaceIcon className="size-3.5" />
+                <span className="text-xs font-medium hidden sm:block">
+                  Remove Color
+                </span>
+              </Button>
+              <Button
+                onClick={handleDelete}
+                className="p-1.5 rounded-lg border border-zinc-700 text-zinc-200 group flex items-center gap-1 data-[hover]:border-zinc-600 transition duration-75 ease-in-out"
+              >
+                <TrashIcon className="size-3.5" />
+                <span className="text-xs font-medium hidden sm:block">
+                  Delete
+                </span>
+              </Button>
+            </>
+          )}
+          {!isEditing && (
+            <>
+              <GithubExport palette={JSON.stringify(item)} />
+              <TailwindExport
+                className={item.name.toLowerCase()}
+                colors={item.colors}
+              />
+            </>
+          )}
+        </div>
+      </div>
+      <div className="w-full flex flex-col sm:flex-row gap-2 relative last:mr-auto">
+        {isEditing
+          ? editPalette.colors.map((color, index) =>
+              renderColorPicker(color, index)
+            )
+          : item.colors.map((color, index) => (
+              <ColorButtonPalette
+                key={`${item.name}-${index}`}
+                color={color}
+                palette={item}
+              />
+            ))}
+      </div>
+    </div>
+  );
+});
