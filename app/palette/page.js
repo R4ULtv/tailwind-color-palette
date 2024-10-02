90d460b8112a84a59f3051b8b6d017c01cc034cb
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-02 16:24:44 +0200

    add local palette

--- app/palette/page.js
+++ app/palette/page.js
@@ -1,8 +1,11 @@
 import { memo } from "react";
+import { Link } from "next-view-transitions";
 import { palette } from "@/lib/palette";
+import { UserIcon } from "@heroicons/react/16/solid";
+
 import SelectFormatMenu from "@/components/ui/SelectFormatMenu";
 import { ColorButtonPalette } from "@/components/ui/ColorButton";
-import TailwindExport from "@/components/ui/TailwindExport";
+import { TailwindExport } from "@/components/ui/TailwindExport";
 
 const PaletteItem = memo(({ item }) => (
   <div className="flex flex-col gap-1.5 border border-zinc-800 hover:border-zinc-700 rounded-xl sm:rounded-2xl p-2">
@@ -47,7 +50,14 @@ PaletteItem.displayName = "PaletteItem";
 export default function PaletteList() {
   return (
     <div className="mt-8 sm:mt-4 pt-4 space-y-3">
-      <div className="flex items-center justify-end gap-2">
+      <div className="flex items-center sm:justify-end justify-center gap-2">
+        <Link
+          href="/palette/local"
+          className="bg-zinc-200 text-zinc-900 px-2 py-1.5 rounded-lg text-sm font-medium flex items-center gap-1 justify-center sm:justify-start"
+        >
+          <UserIcon className="size-4" />
+          Local Palette
+        </Link>
         <SelectFormatMenu />
       </div>
       <div className="grid gap-4 sm:gap-6">
