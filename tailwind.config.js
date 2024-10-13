a4ed5b79167704dd64dc013a0335d9113a89a406
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-13 11:41:07 +0200

    add vscode palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        vscode: {
+          "light-blue": "#9CDCFE",
+          purple: "#C586C0",
+          blue: "#4FC1FF",
+          yellow: "#ffd700",
+          tan: "#CE9178",
+          green: "#6A9955",
+        },
+      },
+    },
+  },
   plugins: [],
 };
