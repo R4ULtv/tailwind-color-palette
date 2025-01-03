9303103a6e31d13a48139539f322ae3defbc6b5a
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-01-03 11:37:54 +0100

    feat: update Select component styles for improved aesthetics and consistency

--- components/ui/select.js
+++ components/ui/select.js
@@ -31,7 +31,7 @@ const SelectContent = React.forwardRef(
     <SelectPrimitive.Portal>
       <SelectPrimitive.Content
         ref={ref}
-        className="relative mt-1 z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-zinc-700 bg-zinc-900 text-zinc-300 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+        className="relative mt-1 z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-xl border border-zinc-700 bg-zinc-900 text-zinc-300 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
         position={position}
         {...props}
       >
@@ -48,7 +48,7 @@ const SelectItem = React.forwardRef(
   ({ className, children, ...props }, ref) => (
     <SelectPrimitive.Item
       ref={ref}
-      className="relative flex w-full cursor-default select-none items-center rounded py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-zinc-800 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+      className="relative flex w-full cursor-default select-none items-center rounded-lg py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-zinc-800 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
       {...props}
     >
       <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
